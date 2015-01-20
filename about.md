af444eb6490899d716d59163b86aa00e6e90f870
Daniel Glunz <dannyglunz@gmail.com>
2015-01-19 22:29:49 -0700

    revert to flip image on scroll, change favicon, add fontawesomeness

--- about.md
+++ about.md
@@ -1,6 +1,6 @@
 ---
 layout: about
-title: 
+title:
 permalink: /about/
 ---
 
@@ -8,8 +8,17 @@ permalink: /about/
 **Name**: Danny Glunz (I like to pretend the L is silent)  
 **Occupation**: Developer in [training](http://turing.io)  
 **Aspirations**: To build useful shit and become a [digital nomad](http://en.wikipedia.org/wiki/Digital_nomad)  
-**Hobbies**: Books, Code, Travel, Startups
+**Hobbies**: Books, Code, Travel, Startups  
+
+##Conference Bio
+ >
+Danny Glunz didn’t merely adopt the internet, he was born on it, moulded by it.
+He has spent a good portion of his life abroad so he pretends to be cultured by
+spelling in the Queen’s English. He recently dropped out of an undergrad in CS
+to work for Rally Software, where he recently quit to continue learning at
+turing.io. What some people would call “commitment issues”, he calls a healthy
+appetite for knowledge and experience.
 
 ##Contact Me
-Send an [email](/contact)  
+Send an [email](/contact)
 I may start using [Twitter](https://twitter.com/dannyglunz) at some point. But don't count on it.
--- assets/js/about.js
+++ assets/js/about.js
@@ -18,23 +18,16 @@ $(function() {
   .mouseleave(function() {
     $( '.about-header' ).css( 'background', '#EDEBED url(http://www.dannyglu.nz/images/away.jpg) no-repeat center center fixed').css('background-size', 'cover' );
   });
-  //
-  // var lastScrollTop = 0;
-  // $(window).scroll(function(event){
-  //    var st = $(this).scrollTop();
-  //    if (st > lastScrollTop){
-  //     $( '.about-header' ).css( 'background', 'url(http://www.dannyglu.nz/images/look.jpg) no-repeat center center fixed').css('background-size', 'cover' );
-  //    } else {
-  //     $( '.about-header' ).css( 'background', 'url(http://www.dannyglu.nz/images/away.jpg) no-repeat center center fixed').css('background-size', 'cover' );
-  //    }
-  //    lastScrollTop = st;
-  // });
-  //
-  $('#more').on('click', function(event) {
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $('#more-about').offset().top
-    }, 1500);
-  });
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+      var st = $(this).scrollTop();
+      if (st > lastScrollTop){
+       $( '.about-header' ).css( 'background', 'url(http://www.dannyglu.nz/images/look.jpg) no-repeat center center fixed').css('background-size', 'cover' );
+      } else {
+       $( '.about-header' ).css( 'background', 'url(http://www.dannyglu.nz/images/away.jpg) no-repeat center center fixed').css('background-size', 'cover' );
+      }
+      lastScrollTop = st;
+   });
 
 });
