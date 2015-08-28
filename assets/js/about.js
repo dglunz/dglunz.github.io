79d8c8c4253211554518248af16ddb291666fe72
Daniel Glunz <dannyglunz@gmail.com>
2015-08-28 11:08:10 -0600

    add the correct js file -- not the compiled one

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,2 +1,23 @@
 $(function() {
+  $('.about-nav .svg-wrapper').hover(function(){
+    $(this).find('.shape').css('stroke-dasharray', 760);
+    $(this).find('.shape').css('stroke-dashoffset', 0);
+  }, function(){
+    $('.about-nav .svg-wrapper .shape').css('stroke-dasharray', "55 350");
+    $('.about-nav .svg-wrapper .shape').css('stroke-dashoffset', -180);
+  });
+
+  checkSize();
+  $(window).resize(checkSize);
+});
+
+function checkSize(){
+  if ($(".breakpoint").css("float") == "right" ){
+    $('.about-nav .svg-wrapper .shape').css('stroke-dasharray', 760);
+    $('.about-nav .svg-wrapper .shape').css('stroke-dashoffset', 0);
+  }
+  else {
+    $('.about-nav .svg-wrapper .shape').css('stroke-dasharray', "55 350");
+    $('.about-nav .svg-wrapper .shape').css('stroke-dashoffset', -180);
+  }
 });
